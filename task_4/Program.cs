3fd07d57c3dbbc532437be4a32eabbeaedb7a5c2
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2022-07-13 16:33:55 +0300

    Added solution to 3, 4 tasks

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да 
+
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = 7;
+int num3 = 23;
+int result = num1 % num2;
+int result2 = num1 % num3;
+
+if(result == 0 & result2 == 0)
+{
+    Console.Write("Кратно");
+}
+else
+{
+    Console.Write("Не кратно");
+}
---
+++ task_5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result1 = num1 * num1;
+int result2 = num2 * num2;
+
+if(result1 == num2 | result2 == num1)
+{
+    Console.Write($"Число является квадратом второго числа");
+}
+else
+{
+    Console.Write("Ошибка");
+}
